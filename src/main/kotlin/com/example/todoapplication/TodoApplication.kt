cd8325177b63e8f2e14d7f8bec4ff176b761cc2d
YJ9352 <act07612@gmail.com>
2024-01-19 07:13:19 +0900

    코드정리, updateStatus 간결하게 개선

--- src/main/kotlin/com/example/todoapplication/TodoApplication.kt
+++ src/main/kotlin/com/example/todoapplication/TodoApplication.kt
@@ -2,7 +2,6 @@ package com.example.todoapplication
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
 class TodoApplication
