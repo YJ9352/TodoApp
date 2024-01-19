734cd81b37684002e4ef1273f0f7d57076c3063a
YJ9352 <act07612@gmail.com>
2024-01-19 11:49:17 +0900

    코드수정

--- src/main/kotlin/com/example/todoapplication/infra/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/todoapplication/infra/security/SecurityConfig.kt
@@ -1,15 +1,9 @@
 package com.example.todoapplication.infra.security
 
-import com.example.todoapplication.domain.user.common.UserRole
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authorization.AuthenticatedAuthorizationManager.authenticated
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
--- src/main/kotlin/com/example/todoapplication/infra/security/jwt/JwtPlugin.kt
+++ src/main/kotlin/com/example/todoapplication/infra/security/jwt/JwtPlugin.kt
@@ -3,7 +3,6 @@ package com.example.todoapplication.infra.security.jwt
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jws
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
